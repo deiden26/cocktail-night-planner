fe26b4200c0cfb20e71a30ef5f0f43f797c13560
Danny Eiden <deiden26@gmail.com>
2022-01-20 16:43:57 -0800

    feat: Replace boilerplate with app skeleton

---
+++ src/Cocktails.js
@@ -0,0 +1,11 @@
+import CocktailList from './CocktailList';
+import CocktailSelector from './CocktailSelector';
+
+export default function Cocktails() {
+  return(
+    <>
+      <CocktailSelector/>
+      <CocktailList/>
+    </>
+  );
+}
---
+++ src/Title.js
@@ -0,0 +1,12 @@
+import Typography from '@mui/material/Typography';
+
+export default function Title() {
+  return (
+    <>
+      <Typography variant="h3" component="h1">Cocktail Night Planner</Typography>
+      <Typography variant="subtitle1">
+        Create shopping lists for your bar
+      </Typography>
+    </>
+  );
+}
