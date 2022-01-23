6fe6ed535571e3085d01f16ce04c9a7072da3037
Danny Eiden <deiden26@gmail.com>
2022-01-23 15:53:54 -0800

    feat: Wire up ingredient list

--- src/IngredientList.js
+++ src/IngredientList.js
@@ -1,23 +1,54 @@
+import { useQueries } from 'react-query';
+import axios from 'axios';
+
 import Paper from '@mui/material/Paper';
 
 import IngredientCard from './IngredientCard';
 import NoIngredientsAlert from './NoIngredientsAlert';
+import { useStore } from './store';
 
 export default function IngredientList() {
-  const ingredients = ['Taquila', 'Rum', 'Lime'];
-  // const ingredients = [];
+  const cocktails = useStore(state => state.cocktails);
+  const allIngredients = new Set();
+
+  const cocktailQueries = useQueries(
+    cocktails.map(cocktail => ({
+      queryKey: ['cocktail-ingredients', cocktail.id],
+      queryFn: async () => {
+        const response = await axios.get(
+          'https://www.thecocktaildb.com/api/json/v1/1/lookup.php',
+          { params: { i: cocktail.id } }
+        );
+        const drink = response.data.drinks[0];
+
+        const ingredients = [];
+        for (let i = 1; i <= 15; i++) {
+          const ingredient = drink[`strIngredient${i}`];
+          if (ingredient) {
+            ingredients.push(ingredient);
+          }
+        }
+        return ingredients;
+      }
+    }))
+  );
+
+  cocktailQueries.forEach(({ data: ingredients }) => {
+    ingredients?.forEach(ingredient => allIngredients.add(ingredient));
+  });
+
 
   return (
     <>
-      {ingredients.length === 0 &&
+      {allIngredients.size === 0 &&
         <NoIngredientsAlert/>
       }
-      {ingredients.length > 0 &&
+      {allIngredients.size > 0 &&
         <Paper
           elevation={1}
           sx={{ width: '100%', height: '100%', p: 2 }}
         >
-          {ingredients.map(ingredient => (
+          {[...allIngredients].map(ingredient => (
             <IngredientCard key={ingredient} name={ingredient}/>
           ))}
         </Paper>
